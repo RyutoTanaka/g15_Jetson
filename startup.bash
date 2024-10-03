497b1c30991225175aacb5e4b92fa4b4dc9b01a0
RyutoTanaka <ryuto.tanaka314@gmail.com>
2024-10-03 19:20:04 +0900

    submoduleを更新

--- startup.bash
+++ startup.bash
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-cd ~/Documents/G15/Jetson
+cd ~/Documents/g15_Jetson/
 . install/setup.bash
 
 modprobe spidev
 
 ros2 run g15_control controller & 
-ros2 topic pub --rate 50 /cmd_vel geometry_msgs/msg/Twist "{linear: {x: 0.1, y: 0.0, z: 0.0}, angular: {x: 0, y: 0.0, z: 0.0}}"
-
+ros2 launch teleop_twist_joy teleop-launch.py 
