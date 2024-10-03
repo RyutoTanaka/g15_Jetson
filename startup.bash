8901da5bd03a45382e763694b4d8f96b0b3dc787
RyutoTanaka <ryuto.tanaka314@gmail.com>
2024-10-03 20:55:22 +0900

    kiks_ai_bridgeを追加しai_serverから動くように変更

--- startup.bash
+++ startup.bash
@@ -5,5 +5,8 @@ cd ~/Documents/g15_Jetson/
 
 modprobe spidev
 
-ros2 run g15_control controller & 
-ros2 launch teleop_twist_joy teleop-launch.py 
+ros2 run g15_control controller __ns:=/g15& 
+ros2 launch teleop_twist_joy teleop-launch.py &
+ros2 run kiks_ai_bridge ai_command __params:=config.yaml __ns:=/g15 &
+
+wait
