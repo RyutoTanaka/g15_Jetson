a6e83ad836be660ef7a0b528028a7587327d21f3
RyutoTanaka <ryuto.tanaka314@gmail.com>
2024-10-07 17:49:49 +0900

    submoduleを更新

--- startup.bash
+++ startup.bash
@@ -6,7 +6,7 @@ cd ~/Documents/g15_Jetson/
 modprobe spidev
 
 ros2 run g15_control controller __ns:=/g15& 
-ros2 launch teleop_twist_joy teleop-launch.py &
+#ros2 launch teleop_twist_joy teleop-launch.py &
 ros2 run kiks_ai_bridge ai_command __params:=config.yaml __ns:=/g15 &
 
 wait
